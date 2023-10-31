0cbfbf6759d24ad6db4818769869387945ed1f3a
Agostina Hebe Espin Borbore <agos.espin@gmail.com>
2023-10-30 22:36:44 -0300

    nuevas actualizaciones
    arreglos de inicio de sesion con microsoft

---
+++ loginBackend/src/app/authConfig.js
@@ -0,0 +1,46 @@
+const msalConfig = {
+    auth: {
+        // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
+        clientId: "Enter_the_Application_Id_Here",
+        // Full directory URL, in the form of https://login.microsoftonline.com/<tenant-id>
+        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
+        // Full redirect URL, in form of http://localhost:3000
+        redirectUri: "Enter_the_Redirect_Uri_Here",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case msal.LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case msal.LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case msal.LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case msal.LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+const tokenRequest = {
+    scopes: ["User.Read", "Mail.Read"],
+    forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
